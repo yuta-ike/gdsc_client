2b06ea24bc7416a2e8a6f7745a86d4226a5c45c1
Tianchen Wang <xiaogeamadeus@gmail.com>
2023-03-26 14:30:53 +0900

    feat: Implement AR snapshot and AR view features

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
+import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart';
 import 'package:flutter/material.dart';
 import './mainTheme.dart';
 import './views/homePage.dart';
 import './views/editRoomPage.dart';
 
-void main() {
+void main() async
+{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  print('CHECK IF ARCORE IS AVAILABLE');
+  print(await ArCoreController.checkArCoreAvailability());
+
+  print('\nCHECK IF AR SERVICES IS INSTALLED');
+  print(await ArCoreController.checkIsArCoreInstalled());
+
   runApp(MyApp());
 }
 
---
+++ lib/model/position.dart
@@ -0,0 +1,11 @@
+class Position {
+  final double xAxisCoordinate;
+  final double yAxisCoordinate;
+  final double zAxisCoordinate;
+
+  Position({
+    required this.xAxisCoordinate,
+    required this.yAxisCoordinate,
+    required this.zAxisCoordinate,
+  });
+}
